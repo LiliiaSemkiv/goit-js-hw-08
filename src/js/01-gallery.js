f544626d759fcd2c4adae49f0bf634bf4748fa8a
LiliiaSemkiv <127901106+LiliiaSemkiv@users.noreply.github.com>
2023-08-21 02:13:55 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+function makeGallery(){
+    const markup = galleryItems
+    .map(({preview, original, description}) => 
+    `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}" ></a></li>`)
+    .join('');
+    gallery.innerHTML = markup
+}
+
+makeGallery();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: "true",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+instance.show()
