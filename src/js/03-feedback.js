f1b603fea894944cefbc4714a903bd29f0ee4870
LiliiaSemkiv <127901106+LiliiaSemkiv@users.noreply.github.com>
2023-08-21 15:51:07 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,38 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form')
-const textarea = document.querySelector("textarea[name='message']")
-const STORAGE_KEY = "feedback-form-state"; 
+const form = document.querySelector('.feedback-form');
+const textareaEl = form.querySelector("textarea[name='message']");
+const emailEl = form.querySelector("input[name='email']");
+const STORAGE_KEY = "feedback-form-state";
+const formData = {
+    email: "",
+    message: ""
+};
 
-
-form.addEventListener('input', throttle(onFormSubmit, 500))
-
-returnTextarea()
+form.addEventListener('input', throttle(onFieldInput, 500));
+form.addEventListener('submit', onFormSubmit);
+window.addEventListener('load', onLoadPage);
 
 function onFormSubmit(e){
     e.preventDefault();
-
-    e.target.reset(); 
-    localStorage.removeItem(STORAGE_KEY)
-    
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
 }
 
-// form.addEventListener('input', onForm)
-
-// function onForm(e){
-//     formData[e.target.email] = e.target.value
-
-//     console.log(formData)
-// }
-
-form.addEventListener('input', onTextareaInput)
-
-form.addEventListener('submit', onTextareaInput)
-
-function onTextareaInput(e){
-    const value = e.target.value;
-    
-    console.log(value)
-
-    localStorage.setItem(STORAGE_KEY, value)
-
-
+function onFieldInput(e) {
+    const { value, name } = e.target;
+    formData[name] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function returnTextarea(){
-    const returnMessage = localStorage.getItem(STORAGE_KEY)
+function onLoadPage() {
+    const returnData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-    if(returnMessage){
-        console.log(returnMessage);
-        textarea.value = returnMessage;
+    if (returnData) {
+        emailEl.value = returnData.email;
+        textareaEl.value = returnData.message;
+        formData.email = returnData.email;
+        formData.message = returnData.message;
     }
 }
