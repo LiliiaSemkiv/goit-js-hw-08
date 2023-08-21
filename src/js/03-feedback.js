c2fc2f8533ec44a34ce7b04ba065aed2895eebfa
LiliiaSemkiv <127901106+LiliiaSemkiv@users.noreply.github.com>
2023-08-21 13:13:41 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form')
+const textarea = document.querySelector("textarea[name='message']")
+const STORAGE_KEY = "feedback-form-state"; 
+
+
+form.addEventListener('input', throttle(onFormSubmit, 500))
+
+returnTextarea()
+
+function onFormSubmit(e){
+    e.preventDefault();
+
+    e.target.reset(); 
+    localStorage.removeItem(STORAGE_KEY)
+    
+}
+
+// form.addEventListener('input', onForm)
+
+// function onForm(e){
+//     formData[e.target.email] = e.target.value
+
+//     console.log(formData)
+// }
+
+form.addEventListener('input', onTextareaInput)
+
+form.addEventListener('submit', onTextareaInput)
+
+function onTextareaInput(e){
+    const value = e.target.value;
+    
+    console.log(value)
+
+    localStorage.setItem(STORAGE_KEY, value)
+
+
+}
+
+function returnTextarea(){
+    const returnMessage = localStorage.getItem(STORAGE_KEY)
+
+    if(returnMessage){
+        console.log(returnMessage);
+        textarea.value = returnMessage;
+    }
+}
